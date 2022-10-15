bbfc7e8ba5743ebb733ea234a5e8ce768e50913d
dohung02 <hungdpps19239@fpt.edu.vn>
2022-10-16 01:19:05 +0700

    Thêm csdl

--- Asm_Java5_Database.sql
+++ Asm_Java5_Database.sql
@@ -85,13 +85,13 @@ GO
 ------------------------- THÊM D? LI?U --------------------------
 -- Insert NguoiDung
 INSERT NguoiDung  (HoTen, NgaySinh, GioiTinh, SoDT ,Email, Anh , TenTK, MatKhau, DiaChi ,VaiTro )
-	VALUES (N'Đỗ Phi Hùng', '2000-01-01', 1, '0383123456', 'hung@gmail.com', 'user1.png','hung123','Hung123', '1234 Quang Trung, Phường 10, Gò Vấp',1)
+	VALUES (N'Đỗ Phi Hùng', '2000-01-01', 1, '0383123456', 'hung@gmail.com', 'user1.png','hung123','Hung123', N'1234 Quang Trung, Phường 10, Gò Vấp',1)
 INSERT NguoiDung  (HoTen, NgaySinh, GioiTinh, SoDT ,Email, Anh , TenTK, MatKhau, DiaChi ,VaiTro )
-	VALUES (N'Cao Hoài Bảo Ngọc', '2000-01-01', 1, '0353123456', 'ngoc@gmail.com', 'user1.png','ngoc123','Ngoc123', '1234 Quang Trung, Phường 10, Gò Vấp',1)
+	VALUES (N'Cao Hoài Bảo Ngọc', '2000-01-01', 1, '0353123456', 'ngoc@gmail.com', 'user1.png','ngoc123','Ngoc123', N'1234 Quang Trung, Phường 10, Gò Vấp',1)
 INSERT NguoiDung  (HoTen, NgaySinh, GioiTinh, SoDT ,Email, Anh , TenTK, MatKhau, DiaChi ,VaiTro )
-	VALUES (N'Bùi Nhật Trường', '2000-01-01', 1, '0363123456', 'truong@gmail.com', 'user1.png','truong123','Truong123', '1234 Quang Trung, Phường 10, Gò Vấp',1)
+	VALUES (N'Bùi Nhật Trường', '2000-01-01', 1, '0363123456', 'truong@gmail.com', 'user1.png','truong123','Truong123', N'1234 Quang Trung, Phường 10, Gò Vấp',1)
 INSERT NguoiDung  (HoTen, NgaySinh, GioiTinh, SoDT ,Email, Anh , TenTK, MatKhau, DiaChi ,VaiTro )
-	VALUES (N'Nguyễn Hoàng Quốc Anh', '2000-01-01', 1, '0373123456', 'anh@gmail.com', 'user1.png','anh123','Anh123', '1234 Quang Trung, Phường 10, Gò Vấp',1)
+	VALUES (N'Nguyễn Hoàng Quốc Anh', '2000-01-01', 1, '0373123456', 'anh@gmail.com', 'user1.png','anh123','Anh123', N'1234 Quang Trung, Phường 10, Gò Vấp',1)
 
 -- Insert DanhMuc
 INSERT INTO DanhMuc VALUES 
@@ -138,11 +138,11 @@ INSERT SanPham (TenSP ,TrangThai ,Anh ,Gia ,SLTonKho ,MoTa ,Id_DM, Is_Delete)
 
 -- Insert DonDatHang
 INSERT DonDatHang(NgayDat ,GhiChuKH ,TongTien, TrangThai, SoDT, DiaChi,ID_KH) 
-	VALUES ('2022-03-30', N'Giao buổi chiều',2400, 1, '0985645231', '357 Lê Văn Thọ, Phường 9, Gò Vấp, TPHCM',1)
+	VALUES ('2022-10-02', N'Giao buổi chiều',2400, N'Đã giao', '0985645231', N'357 Lê Văn Thọ, Phường 9, Gò Vấp, TPHCM',1)
 INSERT DonDatHang(NgayDat ,GhiChuKH ,TongTien, TrangThai, SoDT, DiaChi, ID_KH) 
-	VALUES ('2022-03-31', N'Giao sớm nha má',2700, 0, '0977485234', '200 Đường 3/2, Phường 12, Quận 10, TPHCM', 2)
+	VALUES ('2022-10-10', N'Giao sớm nha má',2700, N'Đang giao', '0977485234', N'200 Đường 3/2, Phường 12, Quận 10, TPHCM', 2)
 INSERT DonDatHang(NgayDat ,GhiChuKH ,TongTien, TrangThai, SoDT, DiaChi, ID_KH)
-	VALUES ('2022-04-02', N'',2600, 1, '0884245251', '686 Trường Chinh, Phường 15, Tân Bình, TPHCM', 2)
+	VALUES ('2022-10-14', N'',2600, N'Chờ xác nhận', '0884245251', N'686 Trường Chinh, Phường 15, Tân Bình, TPHCM', 2)
 
 -- Insert DonDatChiTiet
 INSERT DonDatChiTiet(ID_DDH ,ID_SP, SoLuong ,TongTien ) VALUES ( 1, 1, 2, 2600 )
@@ -155,4 +155,28 @@ INSERT FeedBack (ID_KH , ID_SP ,DanhGia ,NoiDung ,NgayDang )
 INSERT FeedBack (ID_KH , ID_SP ,DanhGia ,NoiDung ,NgayDang ) 
 	VALUES (2, 2, 3, N'Giao hàng lâu', '2022-04-15')
 
+-- Số đơn hàng đã giao
+if OBJECT_ID('spsell') is not null
+	drop proc spsell
+go
 
+create proc spsell
+as 
+	select SUM(ct.soluong) as N'Đơn đã giao' from DonDatChiTiet as ct join DonDatHang as dd 
+	on ct.ID_DDH = dd.ID_DDH where dd.trangthai like N'Đã giao'
+go
+--exec spsell
+
+-- tạo pro chưa xác nhận
+if OBJECT_ID('sp_xacnhan ') is not null
+	drop proc sp_xacnhan 
+go
+create proc sp_xacnhan
+as
+select nd.HoTen,ct.SoLuong,ct.TongTien,dh.TrangThai from  DonDatHang as dh 
+join NguoiDung as nd on dh.ID_KH = nd.ID_KH
+join DonDatChiTiet ct on dh.ID_DDH = ct.ID_DDH
+where dh.TrangThai like N'Chờ xác nhận'
+go
+
+-- exec sp_xacnhan
